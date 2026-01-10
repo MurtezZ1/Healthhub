8c4dcd56520b356067c570c98057908cb8153647
MurtezZ1 <em70057@ubt-uni.net>
2026-01-10 18:18:04 +0100

    krijimi i filev ne migrations

--- ReactApp1.Server/obj/Debug/net10.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1.Server/obj/Debug/net10.0/ReactApp1.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93cd6d9a16afb59a2500707a7b1c6592cad4bba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aeaa1ca20e98a64cf01b1cec5964aac23040dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
